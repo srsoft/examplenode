83e10bd3d9dee6d084123c999c03941cfcb78c70
유성용 <stevegates1013@gmail.com>
2024-02-08 10:08:09 +0900

    5000

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hello World! 123'))
-app.listen(3000, () => console.log('Server ready'))
+app.get('/', (req, res) => res.send('Hello World! 1234'))
+app.listen(5000, () => console.log('Server ready'))
