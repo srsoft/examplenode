078c12ef5a141c9cc9d0624aa8a8cf0992fa3655
Daniel <stevegates1013@gmail.com>
2024-02-17 02:47:30 +0900

    test7

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hello World! 12345 clusterIp 6'))
+app.get('/', (req, res) => res.send('Hello World! 12345 clusterIp 7'))
 app.listen(5000, () => console.log('Server ready'))
