52ab4b112760ee474d7fb50a9e6a24313a8fdc76
Daniel <stevegates1013@gmail.com>
2024-02-17 12:33:39 +0900

    harbor branch

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hello World! 12345 clusterIp 8'))
+app.get('/', (req, res) => res.send('Hello harbor 1'))
 app.listen(3000, () => console.log('Server ready'))
